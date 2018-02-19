45c67b346a832efc88bb29d70b21562cdb78b791
Benji Bear <bearb@umich.edu>
2018-02-19 14:38:17 -0500

    consolidated lab search to just one input

--- frontend/src/components/LabSearch.js
+++ frontend/src/components/LabSearch.js
@@ -11,14 +11,13 @@ class LabSearch extends Component {
 		return (
 			<div className='form labSearch shadow'>
 				<div className='row'>
-					<div className='col s12 m12 l2 left-align lab-search-label'>LAB SEARCH</div>
-					<div className='col s12 m4 l4'><input id='lab-topic' className='lab-search-input' type='text' placeholder='interest keywords' /></div>
-					<div className='col s12 m4 l4'><input id='lab-name' className='lab-search-input' type='text' placeholder='skill keywords' /></div>
-					<div className='col s12 m3 l2'>
+					<div className='col s12 m12 l2 left-align lab-search-label grey-text text-darken-1'>LAB SEARCH</div>
+					<div className='col s12 m7 '><input id='lab-topic' className='lab-search-input' type='text' placeholder='keywords' /></div>
+					<div className='col s12 m3 l3'>
 						<button className="btn waves-effect waves-light submit-btn lab-search-btn"
 			        			type="submit" 
 			        			name="action"
-			        			style={{marginTop: '0px', marginLeft: '15px', backgroundColor: 'rgb(41, 182, 246)', letterSpacing: '2px'}}
+			        			style={{marginTop: '0px', marginLeft: '15px', backgroundColor: '#0277bd', letterSpacing: '2px'}}
 			        		>Search
 			        	</button>
 			        </div>
