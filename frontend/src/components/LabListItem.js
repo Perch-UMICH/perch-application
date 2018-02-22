f19c9e3f432c75ac88c7d09caaac247a6ea90c81
Benji Bear <bearb@umich.edu>
2018-02-22 11:09:17 -0500

    Minor changes to lab match to make it look ok on mmobile

--- frontend/src/components/LabListItem.js
+++ frontend/src/components/LabListItem.js
@@ -12,8 +12,8 @@ class LabListItem extends Component {
 			<div className='lab-list-item shadow'>
 				<a href={this.props.profile_link}><img src={this.props.img} className='lab-list-item-img'/></a>
 				<div className='lab-list-tag-container'>
-					<a className='null-link-style' href={this.props.profile_link}><div className='lab-list-name'>{this.props.labName}</div></a>
-					{this.props.tags.map((tag) => <div key={tag} className='floater-item'>{tag}</div>)}
+					<a className='null-link-style' href={this.props.profile_link}><div className='lab-list-name hide-on-small-only'>{this.props.labName}</div></a>
+					<span className='hide-on-small-only'>{this.props.tags.map((tag) => <div key={tag} className='floater-item'>{tag}</div>)}</span>
 				</div>
 				
 				<div className="lab-list-btn-container"><BasicButton dest={this.props.dest} msg='apply'/></div>
