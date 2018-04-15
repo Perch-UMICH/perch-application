7d6ccadd38962168143f5d7b003083d36dbe1aa7
Emi Jackson <emij@umich.edu>
2018-04-15 17:53:23 -0400

    working on setting lab_id on login (unfinished)

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -86,7 +86,11 @@ export function loginUser(email, password) {
             }
             else if (response.data.result[0].is_faculty) {
                 // sessionStorage.setItem('student_id', null);
-                sessionStorage.setItem('faculty_id', response.data.result[1].id);
+                sessionStorage.setItem('faculty_id', response.data.result[1].id); // EMI HAS CHANGED THIS! FROM HERE TILL...
+                getUserLabs(response.data.result[0].id).then(resp => {
+                    console.log(resp);
+                    // sessionStorage.setItem('lab_id', somethin_good);
+                }); // ... HERE!
             }
             console.log('Successfully logged in');
             return response.data
