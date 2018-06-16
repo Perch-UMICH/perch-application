f9102d24aac08a004a871b4df163fed24c85eebc
Emi Jackson <emij@umich.edu>
2018-06-15 22:03:10 -0700

    updated styling for timeline and terms of service page implemented

--- frontend/src/components/PrivacyPolicy.js
+++ frontend/src/components/PrivacyPolicy.js
@@ -5,48 +5,48 @@ class PrivacyPolicy extends Component {
 	render() {
 		return (
 			<div className='shift-down'>
-				<div className='container privacy-policy-page-container center-align shadow'>
+				<div className='container terms-page-container center-align shadow'>
 					<div className='apply-header'>Perch Privacy Policy</div>
-					<div className="container privacy-policy-text">
-					<div className="privacy-text-centered">Last Updated: 6/12/18</div>
+					<div className="container terms-text">
+					<div className="terms-text-centered">Last Updated: June 15th, 2018</div>
 					<p>
 						This privacy policy (“Policy”) describes how Perch and its related companies (“Company”) collect, use and share personal information of consumer users of this website, perch.research.umich.edu (the “Site”). This Policy also applies to any of our other websites that post this Policy. This Policy does not apply to websites that post different statements.
  					</p>
- 					<p className="privacy-section-title">
+ 					<p className="terms-section-title">
 						WHAT WE COLLECT
 					</p>
  		
 					We get information about you in a range of ways.
 
-					<ul className="privacy-ul">
+					<ul className="terms-ul">
 						 <li >
-							<div className="privacy-li-header">· Information You Give Us.</div>
-							<div className="privacy-li-content">
+							<div className="terms-li-header">· Information You Give Us.</div>
+							<div className="terms-li-content">
 								We collect your‎ name, postal address, email address, phone number, fax number, username, password, demographic information (such as your gender and occupation) as well as other information you directly give us on our Site.
 							</div>
 						</li>
 						<li>
-							<div className="privacy-li-header">· Information We Get From Others.</div>
-							<div className="privacy-li-content">
+							<div className="terms-li-header">· Information We Get From Others.</div>
+							<div className="terms-li-content">
 								We may get information about you from other sources. We may add this to information we get from this Site.
 						 	</div>
 						 </li>
 						 <li>
-							<div className="privacy-li-header">· Information Automatically Collected.</div>
-							<div className="privacy-li-content">
+							<div className="terms-li-header">· Information Automatically Collected.</div>
+							<div className="terms-li-content">
 								We automatically log information about you and your computer. For example, when visiting our Site, we log your computer operating system type, browser type, browser language, the website you visited before browsing to our Site, pages you viewed, how long you spent on a page, access times and information about your use of and actions on our Site.
 							</div>
 						 </li>
 						 <li>
-							<div className="privacy-li-header">· Cookies.</div>
-							<div className="privacy-li-content"> 
+							<div className="terms-li-header">· Cookies.</div>
+							<div className="terms-li-content"> 
 								We may log information using "cookies." Cookies are small data files stored on your hard drive by a website. We may use both session Cookies (which expire once you close your web browser) and persistent Cookies (which stay on your computer until you delete them) to provide you with a more personal and interactive experience on our Site.	This type of information is collected to make the Site more useful to you and to tailor the experience with us to meet your special interests and needs.
 							</div>
 						</li>
 					</ul>
 
 
-					<p className="privacy-section-title">
+					<p className="terms-section-title">
 						USE OF PERSONAL INFORMATION
 					</p>
 
@@ -73,24 +73,42 @@ class PrivacyPolicy extends Component {
 						</li>
 					</ul>
 
-					<p className="privacy-section-title">
+					<p className="terms-section-title">
 					SHARING OF PERSONAL INFORMATION
 					</p>
 
 					We may share personal information as follows:
 					 
-					·   	We may share personal information with your consent. For example, you may let us share personal information with others for their own marketing uses. Those uses will be subject to their privacy policies.
-					·   	We may share personal information when we do a business deal, or negotiate a business deal, involving the sale or transfer of all or a part of our business or assets. These deals can include any merger, financing, acquisition, or bankruptcy transaction or proceeding.
-					·   	We may share personal information for legal, protection, and safety purposes.
-					o   We may share information to comply with laws.
-					o   We may share information to respond to lawful requests and legal processes.
-					o   We may share information to protect the rights and property of Perch, our agents, customers, and others. This includes enforcing our agreements, policies, and terms of use.
-					o   We may share information in an emergency. This includes protecting the safety of our employees and agents, our customers, or any person.
-					·   	We may share information with those who need it to do work for us.
-					 
+					<ul>
+						<li>
+						·   We may share personal information with your consent. For example, you may let us share personal information with others for their own marketing uses. Those uses will be subject to their privacy policies.
+						</li>
+						<li>
+						·   We may share personal information when we do a business deal, or negotiate a business deal, involving the sale or transfer of all or a part of our business or assets. These deals can include any merger, financing, acquisition, or bankruptcy transaction or proceeding.
+						</li>
+						<li>
+						·   We may share personal information for legal, protection, and safety purposes.
+						</li>
+						<li>
+						·   We may share information to comply with laws.
+						</li>
+						<li>
+						·   We may share information to respond to lawful requests and legal processes.
+						</li>
+						<li>
+						·   We may share information to protect the rights and property of Perch, our agents, customers, and others. This includes enforcing our agreements, policies, and terms of use.
+						</li>
+						<li>
+						·   We may share information in an emergency. This includes protecting the safety of our employees and agents, our customers, or any person.
+						</li>
+						<li>
+						·   We may share information with those who need it to do work for us.
+						</li>
+					</ul> 
+
 					We may also share aggregated and/or anonymized data with others for their own uses.
  
-					<p className="privacy-section-title">
+					<p className="terms-section-title">
 						INFORMATION CHOICES AND CHANGES
 					</p>
 					<p>
@@ -101,7 +119,7 @@ class PrivacyPolicy extends Component {
 						You can typically remove and reject cookies from our Site with your browser settings. Many browsers are set to accept cookies until you change your settings. If you remove or reject our cookies, it could affect how our Site works for you.
 					</p>
 
-					<p className="privacy-section-title">
+					<p className="terms-section-title">
 						CONTACT INFORMATION
 					</p>
 					<p>
@@ -113,7 +131,7 @@ class PrivacyPolicy extends Component {
 						Ann Arbor, Michigan 48104<br/>
 					</p>
 					 
-					<p className="privacy-section-title">
+					<p className="terms-section-title">
 						CHANGES TO THIS PRIVACY POLICY. 
 					</p>
 					We may change this privacy policy. If we make any changes, we will change the Last Updated date above.
