15dbb8a962640e1b6bf72955fe791402ec8abc43
Caleb Kaczmarek <cjkaczma@umich.edu>
2018-06-22 06:58:14 -0400

    Add new events up to May 2018 to Timeline

--- frontend/src/components/Timeline.js
+++ frontend/src/components/Timeline.js
@@ -66,28 +66,75 @@ class Timeline extends Component {
 				{/*<CountdownTimer />*/}
 				<section className="timeline">
 				    <div className="container">
+                        <TimelineItem
+                            header='UROP'
+                            date='MAY 2018'
+                            description='Perch collaborates with the UM Undergraduate Research Opportunities Program (UROP)'
+                            linkLabel='UROP'
+                            linkRef='https://lsa.umich.edu/urop'
+                            fadeIn='L'
+                        />
+
+                        <TimelineItem
+                            header='Additional Classes'
+                            date='APR 2018'
+                            description='Perch begins working with the UM Chemical Engineering (Prof. Sharon Glotzer, Chair) and Molecular and Cellular Biology (Prof. Robert Denver, Chair) departments to offer a classes for the Winter 2019 semester'
+                            linkLabel='CHEMICAL ENGINEERING DEPARTMENT'
+                            linkRef='https://che.engin.umich.edu/'
+                            fadeIn='R'
+                        />
+
+                        <TimelineItem
+                            header='First Course Offered'
+                            date='MAR 2018'
+                            description='Perch collaborates with the University of Michigan Chemistry Department to offer a class, CHEM 211H, in the Fall 2018 semester'
+                            linkLabel='CHEMISTRY COURSE CATALOG'
+                            linkRef='https://webapps.lsa.umich.edu/CrsMaint/Public/CB_PublicBulletin.aspx?crselevel=UG&subject=CHEM'
+                            fadeIn='L'
+                        />
+
+				   		 <TimelineItem
+				  			header='Michigan Library Blog Post'
+				  			date='26 FEB 2018'
+				  			description="Perch's progress is recognized by the University of Michigan Library blogs"
+				  			linkLabel='MICHIGAN LIBRARY BLOGS'
+				  			linkRef='https://www.lib.umich.edu/blogs/student-stories/student-mini-grants-perch-equalizing-stem-research-opportunities'
+				  			fadeIn='R'
+				  		/>
+
+				   		 <TimelineItem
+				  			header='Student Startup Madness'
+				  			date='16 JAN 2018'
+				  			description='Perch is recognized as one of the top 32 college startups by the Student Startup Madness Competition'
+				  			linkLabel='STUDENT STARTUP MADNESS'
+				  			linkRef='http://studentstartupmadness.com/2018-field-of-32-in-student-startup-madness-college-entrepreneurship-tournament-announced/?utm_source=Field+of+64&utm_campaign=e2df9380a9-64_Announcement_2017_2018__1_4_2018+-+faculty&utm_medium=email&utm_term=0_94e4d11fd8-e2df9380a9-559801057'
+				  			fadeIn='L'
+				  		/>
+
 					  	<TimelineItem 
 					  		header='Jo Angela Oehrli' 
 				  			date='19 NOV 2017' 
 				  			description='Jo Angela Oehrli, published UM librarian, joins the team as a primary advisor' 
 				  			linkLabel='TODO'
-				  			fadeIn='L'
-				  		/>
-
-				  		<TimelineItem 
-					  		header='U of M Library Minigrant' 
-				  			date='24 SEP 2017' 
-				  			description='Perch wins the University of Michigan library Student Mini-Grant Award' 
-				  			linkLabel='TODO'
 				  			fadeIn='R'
 				  		/>
 
-				  		<TimelineItem 
-					  		header='Splash Page Deployed' 
-				  			date='27 OCT 2017' 
-				  			description='Splash page goes online, including basic platform functionality' 
-				  			linkLabel='TODO'
+				  		<TimelineItem
+				  			header='Michigan Business Challenge'
+				  			date='11 NOV 2017'
+				  			description='Perch qualifies for Round One of the Michigan Business Challenge'
+				  			linkLabel='MICHIGAN BUSINESS CHALLENGE'
+				  			linkRef='http://zli.umich.edu/programs-funds/michigan-business-challenge'
 				  			fadeIn='L'
+				  		/>
+
+				  		<TimelineItem
+				  			header='Innovation in Action'
+				  			date='9 NOV 2017'
+				  			description='Perch participates in the Innovation in Action Education Challenge'
+				  			linkLabel='INNOVATION IN ACTION'
+				  			linkRef='http://innovationinaction.umich.edu/teams/current-teams.html'
+				  			fadeIn='R'
 				  		/>
 
 				  		<TimelineItem 
@@ -95,16 +142,50 @@ class Timeline extends Component {
 				  			date='29 OCT 2017' 
 				  			description='Perch mascot, Rodriguez, developed by graphic design team' 
 				  			linkLabel='TODO'
-				  			fadeIn='R'
+				  			fadeIn='L'
 
 				  		/> 
+
+				  		<TimelineItem 
+					  		header='Splash Page Deployed' 
+				  			date='27 OCT 2017' 
+				  			description='Splash page goes online, including basic platform functionality' 
+				  			linkLabel='TODO'
+				  			fadeIn='R'
+				  		/>
+
+				  		<TimelineItem 
+					  		header='U of M Library Minigrant' 
+				  			date='24 SEP 2017' 
+				  			description='Perch wins the University of Michigan library Student Mini-Grant Award' 
+				  			linkLabel='TODO'
+				  			fadeIn='L'
+				  		/>
 
 				  		<TimelineItem 
 					  		header='Jim Bennett' 
 				  			date='23 AUG 2017' 
 				  			description='Jim Bennett, Microsoft Product lead, joins the team at a primary advisor' 
 				  			linkLabel='TODO'
+				  			fadeIn='R'
+				  		/> 
+
+						<TimelineItem 
+					  		header='The Middle Road' 
+				  			date='23 JUN 2017' 
+				  			description='Perch is featured in The Middle Road blog under an early name, M-STEP' 
+				  			linkLabel='THE MIDDLE ROAD'
+				  			linkRef='https://themiddleroad.org/2017/06/23/social-impact-entrepreneurship-umich-optimize-it-the-best-way-part-1/#more-4481'
 				  			fadeIn='L'
+				  		/> 
+
+				  		<TimelineItem 
+					  		header='Optimize Challenge' 
+				  			date='24 FEB 2017' 
+				  			description='Perch receives Optimize Social Innovation Challenge grant, with our own Akira and Hyejin become Optimize fellows' 
+				  			linkLabel='OPTIMIZE' 
+				  			linkRef='https://www.optimizemi.org/'
+				  			fadeIn='R'
 				  		/> 
 
 				  		<TimelineItem 
@@ -113,15 +194,6 @@ class Timeline extends Component {
 				  			description='Perch wireframe prototype showcased at Optimize prototype night' 
 				  			linkLabel='WIREFRAME' 
 				  			linkRef='https://xd.adobe.com/view/7b2ab11b-723d-4340-af58-ae50727bb6ad/'
-				  			fadeIn='R'
-				  		/> 
-
-				  		<TimelineItem 
-					  		header='Optimize Challenge' 
-				  			date='24 FEB 2017' 
-				  			description='Perch receives Optimize Social Innovation Challenge grant, with our own Akira and Hyejin becoming Optimize fellows' 
-				  			linkLabel='OPTIMIZE' 
-				  			linkRef='https://www.optimizemi.org/'
 				  			fadeIn='L'
 				  		/> 
 
