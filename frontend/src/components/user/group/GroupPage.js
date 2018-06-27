670f0b29b46e6a47b2f1dfe26763c28ce3700116
Benji Bear <bearb@umich.edu>
2018-06-26 23:17:47 -0400

    Updated quickview card on group page

--- frontend/src/components/user/group/GroupPage.js
+++ frontend/src/components/user/group/GroupPage.js
@@ -4,6 +4,18 @@ import {permissionCheck, getLab, isLoggedIn, getCurrentUserId, getUser, getFacul
 import './GroupPage.css'
 
 class ProfPage extends Component {
+
+	expandDescription() {
+        let toggleExpanderIcons = () => {
+            let expanderIcons = document.getElementById(`group-page-expand-icons`)
+            for (let i = 0; i < expanderIcons.children.length; i++)
+                expanderIcons.children[i].innerText = (expanderIcons.children[i].innerText == 'expand_more') ? 'expand_less' : 'expand_more';
+            expanderIcons.classList.toggle('active-blue')
+        }
+        toggleExpanderIcons();
+        alert('todo')
+    }
+
 	render() {
 		return(
 			<div id='group-page'>
@@ -11,7 +23,14 @@ class ProfPage extends Component {
 				<div id='group-page-column-R'>Right Column</div>
 				<div id='group-page-main'>
 					<div id='group-page-quickview'>
-						<img src='https://lh3.googleusercontent.com/laRC-BGx_XtNwr29GCZ3L04qmkxTuZVVpHEPGaSzKcaGP2Nng06ANc-OGad1UWpDlmAtyCByLdRXxqAouiqi6z0h82lNzM8n=w1600-rj' id='group-page-coverimage' />
+						<img src='https://thumbs.dreamstime.com/b/group-students-laboratory-lab-science-classroom-92938916.jpg' id='group-page-coverimage' />
+						<div id='group-page-name'>Dr. Jone's Chemistry Lab</div>
+						<div id='group-page-description'>Our laboratory is very interested in understanding the functional and organizational patterns underlying complex systems with our major emphasis being on protein interactions and the role of post-translational modifications in controlling those interactions.  We rely primarily on mass spectrometry and protein chemistry techniques to address problems in these areas.</div>
+						<div id='group-page-expand-icons' onClick={this.expandDescription.bind(this)}>
+	                        <i className='material-icons'>expand_more</i>
+	                        <i className='material-icons'>expand_more</i>
+	                        <i className='material-icons'>expand_more</i>
+	                    </div>
 					</div>
 					<div id='group-page-projects'>
 						projects
