cac634719ffdab0d10dd5ac2de806f7091e69816
Benji Bear <bearb@umich.edu>
2018-07-20 18:51:36 -0400

    updated saved labs

--- frontend/src/components/labSearch/LabDashboard.js
+++ frontend/src/components/labSearch/LabDashboard.js
@@ -7,24 +7,17 @@ class LabDashboard extends Component {
 	render() {
 		return (
 			<div className='shift-down'>
+				<div id='saved-labs-header'>Saved Labs</div>
 				<div className='lab-dashboard'>
 		           	<div id='lab-srch-results'>
-		           		<a className='lab-dashboard-lab-name'>Dr. Jone's Lab</a>
+		           		<a className='lab-dashboard-lab-name'>Dr. Meha's Lab</a>
 		           		<LabSearchProject title='Oncology Study' spots='1' description='We need you to do stuff on this project. Cause research funding crisis. And we need hands on the job.' urop/>
+		           		<a className='lab-dashboard-lab-name'>Dr. Patel's Lab</a>
+		           		<LabSearchProject title='Pharmaceutical Assistant' spots='2' description='We need you to do stuff on this project. Cause research funding crisis. And we need hands on the job.'/>
 		           		<a className='lab-dashboard-lab-name'>Dr. Jone's Lab</a>
-		           		<LabSearchProject title='Oncology Study' spots='1' description='We need you to do stuff on this project. Cause research funding crisis. And we need hands on the job.' urop/>
-		           		<a className='lab-dashboard-lab-name'>Dr. Jone's Lab</a>
-		           		<LabSearchProject title='Oncology Study' spots='1' description='We need you to do stuff on this project. Cause research funding crisis. And we need hands on the job.' urop/>
-		           		<a className='lab-dashboard-lab-name'>Dr. Jone's Lab</a>
-		           		<LabSearchProject title='Oncology Study' spots='1' description='We need you to do stuff on this project. Cause research funding crisis. And we need hands on the job.' urop/>
-		           		<a className='lab-dashboard-lab-name'>Dr. Jone's Lab</a>
-		           		<LabSearchProject title='Oncology Study' spots='1' description='We need you to do stuff on this project. Cause research funding crisis. And we need hands on the job.' urop/>
-		           		<a className='lab-dashboard-lab-name'>Dr. Jone's Lab</a>
-		           		<LabSearchProject title='Oncology Study' spots='1' description='We need you to do stuff on this project. Cause research funding crisis. And we need hands on the job.' urop/>
-		           		<a className='lab-dashboard-lab-name'>Dr. Jone's Lab</a>
-		           		<LabSearchProject title='Oncology Study' spots='1' description='We need you to do stuff on this project. Cause research funding crisis. And we need hands on the job.' urop/>
-		           		<a className='lab-dashboard-lab-name'>Dr. Jone's Lab</a>
-		           		<LabSearchProject title='Oncology Study' spots='1' description='We need you to do stuff on this project. Cause research funding crisis. And we need hands on the job.' urop/>
+		           		<LabSearchProject title='Lab Analyst' spots='1' description='We need you to do stuff on this project. Cause research funding crisis. And we need hands on the job.'/>
+		           		<a className='lab-dashboard-lab-name'>Dr. Manischevitz's Lab</a>
+		           		<LabSearchProject title='Rat Overlord' spots='2' description='We need you to do stuff on this project. Cause research funding crisis. And we need hands on the job.'/>
 		        	</div>
 		       </div>
 	       </div>
--- frontend/src/components/labSearch/LabSearchProject.js
+++ frontend/src/components/labSearch/LabSearchProject.js
@@ -2,6 +2,16 @@ import React, {Component} from 'react';
 import './LabSearchProject.css';
 
 class LabSearchProject extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            added: true,
+        }
+    }
+
+    toggleAdder = () => {
+        this.setState({added: !this.state.added})
+    }
 
 	render() {
 		return (
@@ -13,6 +23,8 @@ class LabSearchProject extends Component {
                 <div className='lab-srch-project-description'>{this.props.description}</div>
                 <div className='lab-srch-project-apply'><a href='mailto:bearb@umich.edu?subject=Dear Future Master&body=Plz take me into your lab. I beg you'>Apply</a></div>
                 <div className='lab-srch-project-openings'><b>{this.props.spots}</b> {this.props.spots - 1 ? "spots" : "spot"}</div>
+                {this.state.added && <i className='material-icons lab-srch-project-adder' onClick={this.toggleAdder}>add</i>}
+                {!this.state.added && <i className='material-icons lab-srch-project-adder' onClick={this.toggleAdder}>remove</i>}
             </div>
              
 		);
