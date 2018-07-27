259a5b1b43aa2fc7d63e49abb090fe27d5bf335e
Benji Bear <bearb@umich.edu>
2018-07-26 21:31:26 -0400

    Updated quickview so title truncates if too long

--- frontend/src/components/user/group/GroupQuickview.js
+++ frontend/src/components/user/group/GroupQuickview.js
@@ -12,7 +12,7 @@ class GroupQuickview extends Component {
 		return(
 			<div id='group-page-quickview'>
 				<img src='https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/NGTYhyRhgilq4uu1a/videoblocks-doodle-cartoon-animation-of-science-chemistry-physics-astronomy-and-biology-school-education-subject-used-for-presenation-title-in-4k-ultra-hd_sl2xqduzw_thumbnail-full12.png' id='group-page-coverimage' />
-				<div id='group-page-name'>{this.props.title}</div>
+				<div id='group-page-name' className='truncate'>{this.props.title}</div>
 				{this.props.description != 'NULL' && 
 					<div>
 						<div id='group-page-description'>{this.props.description}</div>
